eb768558e9e03cbbf0f3acab2a8b6e97181a3929
Tope Olotu <topeolotu75@gmail.com>
2020-10-22 10:37:32 +0100

    real-time notification

--- src/Today/Today.js
+++ src/Today/Today.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Today.css';
 import axios from 'axios';
+import Pusher from 'pusher-js';
 
 class Today extends Component{
     constructor(){
@@ -16,12 +17,62 @@ class Today extends Component{
         axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
             .then(response => {
                 this.setState({ btcprice: response.data.BTC.USD });
+                localStorage.setItem('BTC', response.data.BTC.USD);
+
                 this.setState({ ethprice: response.data.ETH.USD });
+                localStorage.setItem('ETH', response.data.ETH.USD);
+
                 this.setState({ ltcprice: response.data.LTC.USD });
+                localStorage.setItem('LTC', response.data.LTC.USD);
             })
             .catch(error => {
                 console.log(error)
             })
+
+        // establish a connection to Pusher
+        this.pusher = new Pusher('28c2151777b34f5ecb69', {
+            cluster: 'eu',
+            encrypted: true
+        });
+        // Subscribe to the 'coin-prices' channel
+        this.prices = this.pusher.subscribe('coin-prices');
+    }
+
+    sendPricePusher (data) {
+        axios.post('/prices/new', {
+            prices: data
+        })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    componentDidMount () {
+        if (!navigator.onLine) {
+            this.setState({ btcprice: localStorage.getItem('BTC') });
+            this.setState({ ethprice: localStorage.getItem('ETH') });
+            this.setState({ ltcprice: localStorage.getItem('LTC') });
+        }
+
+        setInterval(() => {
+            axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
+                .then(response => {
+                    this.sendPricePusher (response.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }, 10000)
+
+        // We bind to the 'prices' event and use the data in it (price information) to update the state values, thus, realtime changes
+        this.prices.bind('prices', price => {
+            this.setState({ btcprice: price.prices.BTC.USD });
+            this.setState({ ethprice: price.prices.ETH.USD });
+            this.setState({ ltcprice: price.prices.LTC.USD });
+        }, this);
     }
 
     render(){
